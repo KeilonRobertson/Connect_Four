5c55f804cfab3f5b38b0117fac8d2b0a264e1dfc
Keilon Robertson <43102923+KeilonRobertson@users.noreply.github.com>
2019-09-29 17:34:54 -0400

    Added more functions for implementation file

--- src/Connect_Four.h
+++ src/Connect_Four.h
@@ -7,7 +7,6 @@ using namespace std;
 const int ASIZE = 6 ;
 
 
-
 class Connect_Four{
 public:
 
@@ -15,8 +14,13 @@ public:
 
     void play();
     void displayBoard();
+    void gameWon(string nm);
     void player1_turn();
     void player2_turn();
-    void checkwin(char tok, string name);
 
+    void verticalCheck(char tok, string name);
+    void horizontalCheck(char tok, string name);
+
+    void diagonalCheck_NW_SE(char tok, string name);
+    void diagonalCheck_NE_SW(char tok, string name);
 };
